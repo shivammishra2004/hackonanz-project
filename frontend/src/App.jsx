7c804f42ca3d9e0362b84f08cc7810aa286dd85d
Shivam <kingshivam2004@gmail.com>
2024-10-17 12:28:09 +0530

    changed fetch link in app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -20,7 +20,7 @@ function App() {
 		queryKey: ["authUser"],
 		queryFn: async () => {
 			try {
-				const res = await fetch("/api/auth/me");
+				const res = await fetch("https://hackonanz-project.onrender.com/api/auth/me");
 				const data = await res.json();
 				if (data.error) return null;
 				if (!res.ok) {

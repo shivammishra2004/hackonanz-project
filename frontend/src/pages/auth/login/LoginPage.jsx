97f9c141b959ed8feebcf044b0d73a83e039a20d
shivammishra2004 <67479789+shivammishra2004@users.noreply.github.com>
2024-10-17 11:54:23 +0530

    Update LoginPage.jsx

--- frontend/src/pages/auth/login/LoginPage.jsx
+++ frontend/src/pages/auth/login/LoginPage.jsx
@@ -25,7 +25,7 @@ const LoginPage = () => {
 	} = useMutation({
 		mutationFn: async ({ username, password }) => {
 			try {
-				const res = await fetch("/api/auth/login", {
+				const res = await fetch("https://hackonanz-project.onrender.com/api/auth/login", {
 					method: "POST",
 					headers: {
 						"Content-Type": "application/json",
